e517c9c17437db87d193b86ea2a7fd424d355f31
dwikis <supmarhernanda@gmail.com>
2018-06-06 12:44:45 +0700

    uas

--- form-edit.php
+++ form-edit.php
@@ -16,9 +16,18 @@
 
     }
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <?php
-    include_once('template/head.php');
+    //include_once('template/head.php');
 ?>
 <body>
     
@@ -108,7 +117,7 @@
     </footer>
 </body>
 <?php
-    include_once('template/script.php');
+    //include_once('template/script.php');
 ?>
 </html>
 
