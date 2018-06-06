1df100f1b3b17f447e19ee112020453dbfafc37a
dwikis <supmarhernanda@gmail.com>
2018-06-06 12:23:14 +0700

    uas

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
     class Koneksi{
         private $server = "localhost";
-        private $username = "root";
-        private $password = "";
-        private $db = "mahasiswa_web";
+        private $username = "id6085205_mahasiswa";
+        private $password = "blackmarket";
+        private $db = "id6085205_data_mahasiswa";
 
         function getKoneksi(){
             return new mysqli($this->server, $this->username, $this->password, $this->db);
